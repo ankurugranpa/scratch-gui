6528d89676d4d46fd206ca6514266eafbbbdfa10
ankuru <ankuru.ahahahaha@gmail.com>
2025-04-14 13:47:47 +0900

    fix: [webpackの起動IPを変更した]

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,8 @@ const base = {
     devtool: 'cheap-module-source-map',
     devServer: {
         contentBase: path.resolve(__dirname, 'build'),
-        host: 'localhost',
+        // host: 'localhost',
+        host: '0.0.0.0',
         port: process.env.PORT || 8601
     },
     output: {
